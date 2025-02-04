cab5842527efb70c2086222f4e649f15ee46d115
Eva Deltor <52659131+evadeltor@users.noreply.github.com>
2025-02-04 20:32:11 +0100

    Update main.py

--- GmId Lookup Table Streamlit/main.py
+++ GmId Lookup Table Streamlit/main.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
+import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
 
 from plot_page import plot_page
 from value_page import value_page
@@ -9,6 +11,8 @@ from process_data_page import process_data_page
 from create_own_graph_page import create_own_graph_page
 from lookup_ratios import cross_lookup_page
 from ratio_page import ratio_page
+from plot_saved_graphs_page import plot_saved_graphs_page
+
 st.set_page_config(layout="wide")
 
 # Sidebar for navigation and data file selection
@@ -24,18 +28,30 @@ data_file_option = st.sidebar.selectbox("Select Data File", npz_files)
 data = np.load(data_file_option)
 
 # Add the new page to the sidebar navigation
-page = st.sidebar.selectbox("Select Page", ["Plot", "Get Value", "Ratio", "Create Your Own Graph", "Cross-Lookup",  "Load RAW Data"])
-
-
+page = st.sidebar.selectbox("Select Page", ["Plot", "Get Value","Overlap graphs", "Ratio", "Create Your Own Graph", "Cross-Lookup",  "Load RAW Data"])
 
 # Add a text input for the data name
 data_name = st.sidebar.text_input("Enter Data Name", value="File1")
 
+# Initialize session state for saved data names
+if 'saved_data_names' not in st.session_state:
+    st.session_state.saved_data_names = []
+    st.session_state.saved_graphs = []
+
 # Add a button to save the data
 if st.sidebar.button("Save Data"):
-    st.session_state.save_data = True
-else:
-    st.session_state.save_data = False
+    st.session_state.saved_data_names.append(f"{data_name}.csv")
+    # Save the data to .csv file with the name
+    pd.DataFrame(data).to_csv(f"{data_name}.csv", index=False)
+    
+    # Save graph data
+    graph_data = st.session_state.get('graph_data', {})
+    if graph_data:
+        st.session_state.saved_graphs.append(graph_data)
+        pd.DataFrame(graph_data).to_csv(f"{data_name}_graph.csv", index=False)
+
+# Dropdown to show the names of the saved data
+saved_data_option = st.sidebar.selectbox("Saved Data Names", st.session_state.saved_data_names)
 
 # Show the selected page
 if page == "Plot":
@@ -47,8 +63,10 @@ elif page == "Manage Data":
 elif page == "Load RAW Data":
     process_data_page()
 elif page == "Create Your Own Graph":
-    create_own_graph_page()
+    create_own_graph_page(saved_data_option)  # pass saved_data_option
 elif page == "Cross-Lookup":
     cross_lookup_page(data)
 elif page == "Ratio":
     ratio_page(data_name, data)
+elif page == "Overlap graphs":
+    plot_saved_graphs_page()
