33df5b231ee891360974927f7a326bcaff2fe09d
Eva Deltor <52659131+evadeltor@users.noreply.github.com>
2025-02-04 20:32:55 +0100

    Update plot_page.py

--- GmId Lookup Table Streamlit/plot_page.py
+++ GmId Lookup Table Streamlit/plot_page.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import numpy as np
-import process_data
 import matplotlib.pyplot as plt
-
+import pandas as pd  # Import pandas for data handling
 
 from process_data import dynamic_get_value, get_parameter_values
 
@@ -25,7 +24,6 @@ def plot_page(data):
     st.markdown("##### Implemented by Eva Deltor", unsafe_allow_html=True)
     st.markdown("---", unsafe_allow_html=True)
 
-
     # Create two columns with wider column width for the graph
     col1, col2 = st.columns([3, 4])
 
@@ -43,37 +41,82 @@ def plot_page(data):
         sweep_name = st.selectbox("Select sweep", ['vsb', 'L', 'vds', 'vgs'])
         output = st.selectbox("Select output", ['cgs', 'cgg', 'ft', 'gds', 'gm', 'gmoverid', 'region', 'vds', 'vgs', 'vth', 'i'])
 
-        selected_sweep_values = st.multiselect(f"Select {sweep_name} values", get_parameter_values(sweep_name, params), default=get_parameter_values(sweep_name, params))
+        selected_sweep_values = st.multiselect(
+            f"Select {sweep_name} values",
+            get_parameter_values(sweep_name, params),
+            default=get_parameter_values(sweep_name, params)
+        )
 
         x_axis_values = get_parameter_values(x_axis_name, params)
 
         # Adding sliders for x-axis range
-        x_min = st.slider("X-axis Min", float(x_axis_values.min()), float(x_axis_values.max()), float(x_axis_values.min()))
-        x_max = st.slider("X-axis Max", float(x_axis_values.min()), float(x_axis_values.max()), float(x_axis_values.max()))
+        x_min, x_max = st.slider(
+            "X-axis range",
+            float(x_axis_values.min()),
+            float(x_axis_values.max()),
+            (float(x_axis_values.min()), float(x_axis_values.max()))
+        )
+
+        # Options for log scale at the bottom
+        x_log_scale = st.checkbox("Log scale X axis")
+        y_log_scale = st.checkbox("Log scale Y axis")
 
     with col2:
         # Filtering x_axis_values based on x_min and x_max
         filtered_x_axis_values = [x for x in x_axis_values if x_min <= x <= x_max]
 
-        # Plotting
+        # Prepare data for plotting and saving
+        graph_data = []
         fig, ax = plt.subplots()
-        for sweep_value in selected_sweep_values:
-            gmoverid_values = [
-                dynamic_get_value(output, vsb, L, vds, vgs, x_axis_name, sweep_name, x, sweep_value,data)
-                for x in filtered_x_axis_values
-            ]
 
-            if all(value is not None for value in gmoverid_values):
-                ax.plot(filtered_x_axis_values, gmoverid_values, label=f'{sweep_name}={sweep_value}')
+        for sweep_value in selected_sweep_values:
+            x_plot = []
+            y_plot = []
+
+            for x in filtered_x_axis_values:
+                # Update parameters based on x_axis_name
+                current_params = {
+                    'vsb': vsb,
+                    'L': L,
+                    'vds': vds,
+                    'vgs': vgs
+                }
+                current_params[x_axis_name] = x
+                current_params[sweep_name] = sweep_value
+
+                # Get the output value
+                y = dynamic_get_value(
+                    output,
+                    current_params['vsb'],
+                    current_params['L'],
+                    current_params['vds'],
+                    current_params['vgs'],
+                    x_axis_name,
+                    sweep_name,
+                    x,
+                    sweep_value,
+                    data
+                )
+                if y is not None:
+                    x_plot.append(x)
+                    y_plot.append(y)
+
+            if x_plot and y_plot:
+                # Plot the data
+                ax.plot(x_plot, y_plot, label=f'{sweep_name}={sweep_value}')
+
+                # Store data for saving
+                for xi, yi in zip(x_plot, y_plot):
+                    graph_data.append({
+                        x_axis_name: xi,
+                        output: yi,
+                        sweep_name: sweep_value
+                    })
 
         ax.set_xlabel(x_axis_name)
         ax.set_ylabel(output)
         ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
         ax.grid(True)
-
-        # Options for log scale at the bottom
-        x_log_scale = st.checkbox("Log scale X axis")
-        y_log_scale = st.checkbox("Log scale Y axis")
 
         if x_log_scale:
             ax.set_xscale('log')
@@ -81,3 +124,22 @@ def plot_page(data):
             ax.set_yscale('log')
 
         st.pyplot(fig)
+
+    # Outside the columns
+    # Add a 'Save Graph Data' button and file name input
+    st.markdown("---")
+    st.markdown("### Save Graph Data")
+
+    # Input for file name
+    file_name = st.text_input('Enter file name for graph data', value='graph_data.csv')
+
+    # When 'Save Graph Data' button is clicked
+    if st.button('Save Graph Data'):
+        if graph_data:
+            # Convert graph data to a DataFrame
+            graph_df = pd.DataFrame(graph_data)
+            # Save the DataFrame to CSV
+            graph_df.to_csv(file_name, index=False)
+            st.success(f"Graph data saved to {file_name}")
+        else:
+            st.warning("No graph data to save.")
